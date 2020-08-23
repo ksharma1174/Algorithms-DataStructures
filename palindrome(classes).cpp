d9a9aa4f9be70c6831b7f9d4a2c1b13a538e3681
ksharma1174 <59580717+ksharma1174@users.noreply.github.com>
2020-08-23 19:33:49 +0530

    Add files via upload

---
+++ palindrome(classes).cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+/*class palindrome{
+    public:
+    int getpalindrome(int n){
+         int a,hold,temp;
+         temp=n;
+        while(n!=0){
+
+            a=n%10;
+            hold=(hold*10)+a;
+            n=n/10;
+
+
+            }
+            if(hold==temp){
+                cout<<"The number is palindrome"<<endl;
+
+            }
+            else{
+                cout<<"The number is not palindrome"<<endl;
+        }
+        return 0;
+    }
+
+};
+*/
+int main(){
+    /*int num;
+    cin>>num;
+    palindrome createpalindrome;
+    cout<<createpalindrome.getpalindrome(num);
+    return 0;
+    */
+
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+
+    cout<<"The answer is:"<<a+b;
+
+    return 0;
+
+
+}
